a8da1344df190d99885178b0e1d0a5a628d21184
jimd1989 <jamesdalrymple@protonmail.com>
2021-08-24 07:20:13 +1000

    cleanup

--- src/EyeD3Tag.hs
+++ src/EyeD3Tag.hs
@@ -4,11 +4,8 @@ module EyeD3Tag where
 -- Each of its command-line flags has a corresponding instance in EyeD3Tag.
 
 import Prelude (Show, String, ($), show)
-import Data.Semigroup (Semigroup(..))
-import Helpers ((◇))
-
-wrap ∷ String → String
-wrap α = "\"" ◇ α ◇ "\""
+import Data.List (filter)
+import Helpers ((◇), (≠))
 
 data EyeD3Tag = Artist       String
               | AlbumArtist  String
@@ -18,23 +15,16 @@ data EyeD3Tag = Artist       String
               | TrackNum     String
               | Title        String
               | Year         String
-              | All          String
-              | Empty
 
 instance Show EyeD3Tag where
-  show (Artist      α) = "-a " ◇ (show $ wrap α)
-  show (AlbumArtist α) = "-b " ◇ (show $ wrap α)
-  show (Album       α) = "-A " ◇ (show $ wrap α)
-  show (DiscNum     α) = "-d " ◇ (show α)
-  show (Genre       α) = "-G " ◇ (show $ wrap α)
-  show (TrackNum    α) = "-n " ◇ (show α)
-  show (Title       α) = "-t " ◇ (show $ wrap α)
-  show (Year        α) = "-Y " ◇ (show α)
-  show (All         α) = show α
-  show (Empty        ) = ""
-
-instance Semigroup EyeD3Tag where
-  α <> ω = All (show α ◇ show ω)
+  show (Artist      α) = "-a " ◇ (show α) ◇ " "
+  show (AlbumArtist α) = "-b " ◇ (show α) ◇ " "
+  show (Album       α) = "-A " ◇ (show α) ◇ " "
+  show (DiscNum     α) = "-d " ◇ (filter (≠ '"') $ show α) ◇ " "
+  show (Genre       α) = "-G " ◇ (show α) ◇ " "
+  show (TrackNum    α) = "-n " ◇ (filter (≠ '"') $ show α) ◇ " "
+  show (Title       α) = "-t " ◇ (show α) ◇ " "
+  show (Year        α) = "-Y " ◇ (filter (≠ '"') $ show α) ◇ " "
 
 -- A newtype describing the constructor of an EyeD3Tag. Sits inside a Matcher
 -- and is invoked on each filename to parse text into a proper ID3 tag.
--- src/FormatString.hs
+++ src/FormatString.hs
@@ -2,12 +2,11 @@ module FormatString where
 
 -- Takes a string representing the tag format of the filenames to be tagged.
 -- Example: "{d}-{n}. {a} - {t}.mp3"
--- Returns a list of Matchers to handle the tag parsing of each filename.
 
-import Prelude (Char, Eq, Int, String, (.), (-), ($), (*>), (==), (>>=))
-import Control.Applicative ((<|>), liftA2, pure)
-import Control.Monad (join)
+import Prelude (Char, Int, Show, String, (.), (+), (-), ($), (>>=), show)
+import Control.Applicative ((<|>), (*>), liftA2, pure)
 import Data.Bitraversable (bisequence)
+import Data.Eq (Eq, (==))
 import Data.Functor (($>))
 import Data.List (filter, foldr1, length, intercalate, zipWith)
 import Data.Maybe (Maybe(..))
@@ -17,12 +16,14 @@ import Text.Parsec.Char (anyChar, char, digit, oneOf, string)
 import Text.Parsec.Combinator (between, choice, count, lookAhead, optionMaybe)
 import EyeD3Tag (EyeD3Tag(..), Tagger(..))
 import Helpers ((⊙), (◇))
-import Matcher (Delimeter(..), Matcher(..))
 
+newtype Delimeter = Delimeter { getDelimeter ∷ Maybe Char }
+  deriving (Eq, Show)
+
+-- Helpers
 tagger ∷ Stream s m Char ⇒ Char → (String → EyeD3Tag) → ParsecT s u m Tagger
 tagger α ω = char α $> Tagger ω
 
--- Helpers
 nextChar ∷ Stream s m Char ⇒ ParsecT s u m Char
 nextChar = anyChar *> anyChar
 
@@ -43,47 +44,26 @@ untilChar α = try $ manyTill anyChar (lookAhead $ char α)
 untilEof ∷ Stream s m Char ⇒ ParsecT s u m String
 untilEof = many1 anyChar
 
--- Old generators
-exactText ∷ Stream s m Char ⇒ ParsecT s u m Matcher
-exactText = ExactText ⊙ (untilChar '{' <|> untilEof)
+-- Generators
+exactText ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m String)
+exactText = plaintext ⊙ (untilChar '{' <|> untilEof)
 
-textTag ∷ Stream s m Char ⇒ ParsecT s u m Matcher
-textTag = liftA2 (uncurry . Until) (check *> choices) delimeterAndCount
+textTag ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m String)
+textTag = check *> liftA2 (uncurry . text) choices delimeterAndCount
   where chars        = "abAGt"
         constructors = [Artist, AlbumArtist, Album, Genre, Title]
         check        = lookAhead $ oneOf chars
         choices      = choice $ zipWith tagger chars constructors
 
-numTag ∷ Stream s m Char ⇒ ParsecT s u m Matcher
-numTag = liftA2 Number (check *> choices) delimeter
+numTag ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m String)
+numTag = number ⊙ (check *> choices)
   where chars        = "dnY"
         constructors = [DiscNum, TrackNum, Year]
         check        = lookAhead $ oneOf chars
         choices      = choice $ zipWith tagger chars constructors
 
-tag ∷ Stream s m Char ⇒ ParsecT s u m Matcher
+tag ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m String)
 tag = between (char '{') (char '}') $ numTag <|> textTag
-
--- New generators
-exactText' ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m EyeD3Tag)
-exactText' = plaintext ⊙ (untilChar '{' <|> untilEof)
-
-textTag' ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m EyeD3Tag)
-textTag' = check *> liftA2 (uncurry . text) choices delimeterAndCount
-  where chars        = "abAGt"
-        constructors = [DiscNum, TrackNum, Year]
-        check        = lookAhead $ oneOf chars
-        choices      = choice $ zipWith tagger chars constructors
-
-numTag' ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m EyeD3Tag)
-numTag' = number ⊙ (check *> choices)
-  where chars        = "dnY"
-        constructors = [DiscNum, TrackNum, Year]
-        check        = lookAhead $ oneOf chars
-        choices      = choice $ zipWith tagger chars constructors
-
-tag' ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m EyeD3Tag)
-tag' = between (char '{') (char '}') $ numTag' <|> textTag'
 
 -- Generated
 untilDelimeter ∷ Stream s m Char ⇒ Delimeter → ParsecT s u m String
@@ -91,19 +71,19 @@ untilDelimeter α = case (getDelimeter α) of
   (Just  ω) → untilChar ω
   (Nothing) → untilEof
 
-text ∷ Stream s m Char ⇒ Tagger → Delimeter → Int → ParsecT s u m EyeD3Tag
+text ∷ Stream s m Char ⇒ Tagger → Delimeter → Int → ParsecT s u m String
 text f α n = eyeD3Tag ⊙ (delimeterCount α >>= nFields)
-  where eyeD3Tag  = runTagger f . intercalate ""
-        nFields ω = count (ω - n) $ untilDelimeter α
+  where eyeD3Tag  = show . runTagger f . intercalate ""
+        nFields ω = count (1 + (ω - n)) $ untilDelimeter α
 
-number ∷ Stream s m Char ⇒ Tagger → ParsecT s u m EyeD3Tag
+number ∷ Stream s m Char ⇒ Tagger → ParsecT s u m String
 number f = eyeD3Tag ⊙ (many1 digit)
-  where eyeD3Tag = runTagger f
+  where eyeD3Tag = show . runTagger f
 
-plaintext ∷ Stream s m Char ⇒ String → ParsecT s u m EyeD3Tag
-plaintext α = string α $> Empty
+plaintext ∷ Stream s m Char ⇒ String → ParsecT s u m String
+plaintext α = string α $> ""
 
 -- The whole thing
 -- Compiles, but not quite accurate with delimeters
-formatString ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m EyeD3Tag)
-formatString = (foldr1 (◇)) ⊙ (many1 $ tag' <|> exactText')
+formatString ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m String)
+formatString = (foldr1 (◇)) ⊙ (many1 $ tag <|> exactText)
