0e0de34b0afcca1ce6300c60d050e3db013b5a33
jimd1989 <jamesdalrymple@protonmail.com>
2021-08-25 07:48:04 +1000

    FileParser newtype

--- src/FormatString.hs
+++ src/FormatString.hs
@@ -1,17 +1,19 @@
-module FormatString where
+module FormatString (FileParser(..), fileParser) where
 
--- Takes a string representing the tag format of the filenames to be tagged.
+-- Parses a string representing the tag format of the filenames to be tagged.
 -- Example: "{d}-{n}. {a} - {t}.mp3"
--- Returns a parser that can run over any filename according to this pattern.
+-- Returns another parser, FileParser, to be run across all mp3 files.
 
 import Prelude (Char, Eq, Int, Show, String, (.), (-), ($), (==), (>>=), show)
 import Control.Applicative ((<|>), (*>), liftA2, pure)
+import Control.Arrow ((+++), left)
 import Data.Bitraversable (bisequence)
+import Data.Either (Either)
 import Data.Functor (($>))
 import Data.List (filter, foldr1, length, intercalate, zipWith)
 import Data.Maybe (Maybe(..))
 import Data.Tuple (uncurry)
-import Text.Parsec (ParsecT, Stream, many1, manyTill, try)
+import Text.Parsec (ParsecT, Stream, many1, manyTill, parse, try)
 import Text.Parsec.Char (anyChar, char, digit, oneOf, string)
 import Text.Parsec.Combinator (between, choice, count, lookAhead, optionMaybe)
 import EyeD3Tag (EyeD3Tag(..), Tagger(..), getTag)
@@ -19,6 +21,8 @@ import Helpers ((⊙), (◇))
 
 newtype Delimeter = Delimeter { getDelimeter ∷ Maybe Char }
   deriving (Eq, Show)
+
+newtype FileParser = FileParser { runParser ∷ String → Either String String }
 
 -- Helpers
 nextChar ∷ Stream s m Char ⇒ ParsecT s u m Char
@@ -48,7 +52,7 @@ delimeterAndCount = bisequence (delimeter, delimeter >>= delimeterCount)
 tagger ∷ Stream s m Char ⇒ Char → (String → EyeD3Tag) → ParsecT s u m Tagger
 tagger α ω = char α $> Tagger ω
 
--- Generators
+-- Format string parsers
 exactText ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m String)
 exactText = plaintext ⊙ (untilChar '{' <|> untilEof)
 
@@ -69,7 +73,7 @@ numTag = number ⊙ (check *> choices)
 tag ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m String)
 tag = between (char '{') (char '}') $ numTag <|> textTag
 
--- Generated
+-- Generated file name parsers
 plaintext ∷ Stream s m Char ⇒ String → ParsecT s u m String
 plaintext α = string α $> ""
 
@@ -87,6 +91,11 @@ number ∷ Stream s m Char ⇒ Tagger → ParsecT s u m String
 number f = getTag f ⊙ (many1 digit)
 
 -- The whole thing
--- Seems to work, needs testing
 formatString ∷ Stream s m Char ⇒ ParsecT s u m (ParsecT s u m String)
 formatString = (foldr1 (◇)) ⊙ (many1 $ tag <|> exactText)
+
+fileParser ∷ String → Either String FileParser
+fileParser = (show +++ makeParser) . parse formatString outerMsg
+  where outerMsg     = "Invalid format string"
+        innerMsg     = "File doesn't match format string"
+        makeParser α = FileParser $ (left show . parse α innerMsg)
