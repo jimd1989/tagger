a93c671bb32f4cc4f4d3152b33c3fb14cda3d16a
jimd1989 <jamesdalrymple@protonmail.com>
2021-08-26 18:35:02 +1000

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
-import Prelude (IO, putStrLn)
+import Prelude (Either, IO, String, ($), (>>=), pure, putStrLn)
+import Control.Monad.Except (ExceptT(..), lift, liftEither, runExceptT)
+import Data.List (drop)
+import System.Environment (getArgs)
+import Helpers ((⊙), head')
+import Parser (FileParser(..), fileParser)
+
+runProgram ∷ IO (Either String ())
+runProgram = runExceptT $ do
+  args         ← lift getArgs
+  formatString ← liftEither $ head' args
+  files        ← pure $ drop 1 args
+  parser       ← liftEither $ fileParser formatString 
+  pure () 
 
 main ∷ IO ()
 main = putStrLn "x"
